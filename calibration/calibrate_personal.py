a1eee4a83c5d2e67e64a4faff625d88ef8b0685f
Ирина Абдуллаева <a.irene.a@mail.ru>
2021-12-10 22:51:44 +0300

    calibration procedure created

--- calibration/calibrate_personal.py
+++ calibration/calibrate_personal.py
@@ -7,6 +7,7 @@ from typing import Union
 from pathlib import Path
 
 from monitor import Monitor
+from io_utils import VideoRecorder
 
 import logging_handler
 logger = logging_handler.get_logger(__name__)
@@ -14,12 +15,15 @@ logger = logging_handler.get_logger(__name__)
 
 class PersonCalibrator:
 
+    available_directions = ['l', 'r', 'u', 'd']
+
     def __init__(self, make_grid: bool = False, grid_size: int = 16):
         """
         Data collection & organization for final Person-specific Adaptation of Gaze Network.
         """
         self._root_dir = Path(__file__).resolve().parent.parent
         self._monitor = Monitor()
+        self._video_recorder = VideoRecorder()
         self._monitor_w = self._monitor.get_width_pixels()
         self._monitor_h = self._monitor.get_height_pixels()
 
@@ -30,7 +34,35 @@ class PersonCalibrator:
         """
         Calibration with desired number of samples (default is 9, enough for good precision).
         """
-        pass
+        # Create a window as a placeholder for images
+        cv2.namedWindow("Calibration_image", cv2.WINDOW_NORMAL)
+        cv2.setWindowProperty("Calibration_image", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+        calibration_data = {'frames': [],
+                            'g_targets': []}
+        point_num = 0
+        while point_num < num_point_calibrate:
+            self._video_recorder.start()
+            direction = random.choice(self.available_directions)
+            image_to_show, g_target = self.create_calibration_image(point_num=point_num,
+                                                                    direction=direction)
+            cv2.imshow('Calibration image', image_to_show)
+            key_press = cv2.waitKey(0)
+            if key_press == ord(direction):
+                logger.info(f"Right direction pressed, left {num_point_calibrate - point_num} calibration points.")
+                self._video_recorder.stop()
+                calibration_data['frames'].append(self._video_recorder.get_frames())
+                calibration_data['g_t'].append(g_target)
+                point_num += 1
+            # Q to quit
+            elif key_press & 0xFF == ord('q'):
+                logger.info("Stopping calibration...")
+                self._video_recorder.stop()
+                cv2.destroyAllWindows()
+                break
+            else:
+                logger.info(f"Wrong direction pressed, try again.")
+                self._video_recorder.stop()
 
     def collect_data(self, num_points_show: int = 20):
         """
@@ -39,7 +71,7 @@ class PersonCalibrator:
         """
         pass
 
-    def create_calibration_image(self, point_num: int):
+    def create_calibration_image(self, point_num: int, direction: str):
         """
         Generate stimulus image with random point for calibration.
         :return: image to show and 3D gaze target
@@ -62,5 +94,30 @@ class PersonCalibrator:
         # Compute the ground truth point of regard
         x_cam, y_cam, z_cam = self._monitor.monitor_to_camera(x, y)
         g_target = (x_cam, y_cam)
+
+        # normal size sans-serif font
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        # BGR color space
+        color = [0, 0, 0]
+        # Image to show
+        image = np.ones((self._monitor_h, self._monitor_w, 3), np.float32)
+        # Flip horizontally
+        if direction == 'r' or direction == 'l':
+            if direction == 'r':
+                cv2.putText(image, "R", (x, y), font, 0.5, color, 2, cv2.LINE_AA)
+            elif direction == 'l':
+                cv2.putText(image, "L", (self._monitor_w - x, y), font, 0.5, color, 2, cv2.LINE_AA)
+                image = cv2.flip(image, 1)
+        #  Flip vertically - ???
+        elif direction == 'u' or direction == 'd':
+            image_trf = np.ones((self._monitor_w, self._monitor_h, 3), np.float32)
+            if direction == 'd':
+                cv2.putText(image_trf, "D", (y, x), font, 0.5, color, 2, cv2.LINE_AA)
+            elif direction == 'u':
+                cv2.putText(image_trf, "U", (self._monitor_h - y, x), font, 0.5, color, 2, cv2.LINE_AA)
+                image_trf = cv2.flip(image_trf, 1)
+            image = image_trf.transpose((1, 0, 2))
+
+        return image, g_target
 
 
--- calibration/io_utils.py
+++ calibration/io_utils.py
@@ -12,14 +12,25 @@ class VideoRecorder:
     """
 
     def __init__(self, camera_idx: int = 0):
-        self.__capture = cv2.VideoCapture(camera_idx)
+        self.__camera_idx = camera_idx
         self.__capture_thread = None
+        self.__capture = None
         self.__frames = []
         self.__retval = True
         self.__stopped = False
 
     def start(self):
+        if not self.__stopped:
+            logger.error(f"Previous VideoCapture thread is still running!")
+            logger.error("Stopping...")
+            self.stop()
+
         logger.info(f"Starting VideoCapture thread...")
+        # Reset variables
+        self.__capture = cv2.VideoCapture(self.__camera_idx)
+        self.__stopped = False
+        self.__frames = []
+        # Run thread
         self.__capture_thread = Thread(target=self.__get_frame, args=())
         self.__capture_thread.start()
         logger.info(f"VideoCapture thread running...")
@@ -43,9 +54,16 @@ class VideoRecorder:
         """
         Stops everything: VideoCapture, thread.
         """
+        if self.__stopped:
+            logger.warning("Already stopped.")
         self.__stopped = True
-        self.__capture.release()
-        self.__capture_thread.join()
+        if self.__capture is not None:
+            logger.info("Stopping VideoCapture...")
+            self.__capture.release()
+        if self.__capture_thread is not None:
+            logger.info("Stopping thread...")
+            self.__capture_thread.join()
+            self.__capture_thread = None
 
     def get_frames(self):
         """
